6b92d7ee286dfa25fabc57afc1380a5666d44510
marcusg1k <marcusgutierrezufc@gmail.com>
2023-03-13 02:45:40 -0500

    weight tracking

--- Marcus/skinnybitch.cpp
+++ Marcus/skinnybitch.cpp
@@ -1,6 +1,24 @@
-#include <iostream> 
+// SUPER GIGA BIG WORKOUT TRACKING
 
+#include <iostream> 
+using namespace std; 
+
+void split(string muscle, int progress)
+{
+    cout << "\nLift: " << muscle << "\nCurrent PR: " << progress;  
+} 
 int main() 
 {
-    std::cout << "too big too strong\n"; 
+    split("BENCH", 255); 
+    "\n";
+    split("DB PRESS", 105); 
+    "\n"; 
+    split("DEADLIFT", 350);
+    "\n";
+    split("SQUAT", 225);  
+    "\n";
+    split("SEATED ROWS:", 160);
+
+
+    return 0; 
 }
