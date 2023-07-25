0d1c5569815ac03783f8a9c4479ca6ec791d10b9
Marcus <marcusgutierrezufc@gmail.com>
2023-07-25 13:24:47 -0500

    lab 6 complete?

--- Marcus/LabAssignment6/LabAssignment06.java
+++ Marcus/LabAssignment6/LabAssignment06.java
@@ -1,5 +1,11 @@
 package LabAssignment6;
 
 public class LabAssignment06 {
-    
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan", "GTR", 2022);
+
+        System.out.println("Make: " + myCar.getMake());
+        System.out.println("Model: " + myCar.getModel());
+        System.out.println("Year: " + myCar.getYear());
+    }
 }
