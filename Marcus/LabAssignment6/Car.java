2e55e57fd846f12961b8f3f34cbdbdb94426ae90
Marcus <marcusgutierrezufc@gmail.com>
2023-07-25 13:25:08 -0500

    complete?

--- Marcus/LabAssignment6/Car.java
+++ Marcus/LabAssignment6/Car.java
@@ -1,5 +1,25 @@
 package LabAssignment6;
 
 public class Car {
-    
+    public String make;
+    public String model;
+    public int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
