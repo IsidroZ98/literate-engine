0e70f3f0db82498d18d1ad2139f96a6c8efe62db
Marcus <marcusgutierrezufc@gmail.com>
2023-07-26 13:05:08 -0500

    lab 6 done

--- Marcus/LabAssignment6/Car.java
+++ Marcus/LabAssignment6/Car.java
@@ -1,25 +1,23 @@
-package LabAssignment6;
+package LabAssignment6; 
 
 public class Car {
-    public String make;
-    public String model;
-    public int year;
-
-    public Car(String make, String model, int year) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
+    String make; 
+    String model; 
+    int year; 
+    
+    public Car(String make, String model, int year) 
+    {
+        this.make = make; 
+        this.model = model; 
+        this.year = year; 
     }
-
-    public String getMake() {
-        return make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getYear() {
-        return year;
-    }
+        public String getMake() {
+            return make; 
+        }
+        public String getModel() {
+            return model; 
+        }
+        public int getYear() {
+            return year; 
+        }
 }
--- Marcus/LabAssignment6/LabAssignment06.java
+++ Marcus/LabAssignment6/LabAssignment06.java
@@ -2,7 +2,7 @@ package LabAssignment6;
 
 public class LabAssignment06 {
     public static void main(String[] args) {
-        Car myCar = new Car("Nissan", "GTR", 2022);
+        Car myCar = new Car("Honda", "Civic", 2017);
 
         System.out.println("Make: " + myCar.getMake());
         System.out.println("Model: " + myCar.getModel());
